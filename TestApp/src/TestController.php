bc139b44ae8a6b33150402d03e32e113ce1cb2de
Mikhail Foye <wasdbit@gmail.com>
2024-06-07 00:08:30 +0500

    :white_check_mark: добавлены файлы для фронтэнда и новый путь

--- TestApp/src/TestController.php
+++ TestApp/src/TestController.php
@@ -2,13 +2,58 @@
 
 namespace TestApp;
 
-use Core\{Route, Response, AbstractController};
+use Core\Router\Route;
+use Core\AbstractController;
+use Core\Http\DefaultResponse;
+use Core\Http\Request;
+use Core\Http\PostRequest;
+use Core\Http\TemplateResponse;
+use Twig\Template;
 
 class TestController extends AbstractController
 {
-    #[Route(path: "/", methods: ["GET"])]
-    function test_hello(): Response
+    #[Route(path: "/", methods: ["GET", "POST"], name: "test_hello_route_name")]
+    function test_hello(): DefaultResponse
     {
-        return new Response("Hello, World!");
+        return new DefaultResponse("Hello, World!");
+    }
+
+    #[Route(path: "list", methods: ["GET", "POST"], name: "test_list")]
+    function test_list(): DefaultResponse
+    {
+        return new DefaultResponse("List, World!");
+    }
+
+    #[Route(path: "/create/asd", methods: ["GET", "POST"], name: "test_create")]
+    function test_create(): DefaultResponse
+    {
+        return new DefaultResponse("Create, World!");
+    }
+
+    #[Route(path: "/post", methods: ["POST"], name: "test_post")]
+    function test_post(PostRequest $request): DefaultResponse
+    {
+        return new DefaultResponse(json_encode($request->payload));
+    }
+
+    #[Route(path: "/redirect", methods: ["GET"], name: "test_redirect")]
+    function test_redirect(Request $request): DefaultResponse
+    {
+        return new DefaultResponse("Redirect, World!", 302, ['Location' => '/']);
+    }
+
+    #[Route(path: "/template", methods: ["GET"], name: "test_template")]
+    function test_render(Request $request): TemplateResponse
+    {
+        return new TemplateResponse(
+            'test.html.twig',
+            ['name' => 'Test']
+        );
+    }
+
+    #[Route(path: "/auth", methods: ["GET"], name: "test_auth")]
+    function test_auth(Request $request): TemplateResponse
+    {
+        return new TemplateResponse("authorization.twig", []); 
     }
 }
