e1e6209244dce9d86f00f28e81b33b59eccd6930
Ilya Filippov <ilyafilippov@MacBook-Pro-Ilya.local>
2024-06-06 23:25:29 +0500

    feat: :sparkles: реализовал реквесты

--- TestApp/src/TestController.php
+++ TestApp/src/TestController.php
@@ -2,13 +2,35 @@
 
 namespace TestApp;
 
-use Core\{Route, Response, AbstractController};
+use Core\Router\Route;
+use Core\AbstractController;
+use Core\Http\Response;
+use Core\Http\Request;
+use Core\Http\PostRequest;
 
 class TestController extends AbstractController
 {
-    #[Route(path: "/", methods: ["GET"])]
+    #[Route(path: "/", methods: ["GET", "POST"], name: "test_hello_route_name")]
     function test_hello(): Response
     {
         return new Response("Hello, World!");
+    }
+
+    #[Route(path: "list", methods: ["GET", "POST"], name: "test_list")]
+    function test_list(): Response
+    {
+        return new Response("List, World!");
+    }
+
+    #[Route(path: "/create/asd", methods: ["GET", "POST"], name: "test_create")]
+    function test_create(): Response
+    {
+        return new Response("Create, World!");
+    }
+
+    #[Route(path: "/post", methods: ["POST"], name: "test_post")]
+    function test_post(PostRequest $request): Response
+    {
+        return new Response(json_encode($request->payload));
     }
 }
