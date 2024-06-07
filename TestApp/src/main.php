2b9eba51da07373c6a7ef5cf1f8dba05f6075170
Mikhail Foye <wasdbit@gmail.com>
2024-06-07 11:44:25 +0500

    :bug: поправил main файл

--- TestApp/src/main.php
+++ TestApp/src/main.php
@@ -1,15 +1,9 @@
 <?php
 
-use Core\Kernel;
 use Core\RequestFactory;
 
 function main(): void
 {
     require_once __DIR__ . '/vendor/mvc_core/mvc/src/Kernel.php';
-
     $request = RequestFactory::MakeRequestFromGlobals();
-
-    // $response = $kernel->handleRequest($request);
-
-    // var_dump($response);
 }
